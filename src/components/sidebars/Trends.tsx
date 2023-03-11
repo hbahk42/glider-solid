7eff334135d027c9087a1596343a012424dc50f1
BAHK Hyeongjun <hbahk42@outlook.com>
2023-03-11 15:15:56 +0900

    For Loop

--- src/components/sidebars/Trends.tsx
+++ src/components/sidebars/Trends.tsx
@@ -1,33 +1,37 @@
+import { For } from "solid-js";
+
+const generateRandomNumber = () => Math.floor(Math.random() * 500);
+
 const trends = [
   {
     category: "Sports",
     content: "Some team won somthing!",
-    glideCount: 300
+    glideCount: generateRandomNumber()
   },
   {
     category: "Finance",
     content: "Bitcoin down again!",
-    glideCount: 200
+    glideCount: generateRandomNumber()
   },
   {
     category: "PC & Games",
     content: "New Eincode game out!",
-    glideCount: 300
+    glideCount: generateRandomNumber()
   },
   {
     category: "Economy",
     content: "It's goin well...",
-    glideCount: 500
+    glideCount: generateRandomNumber()
   },
   {
     category: "Celebrities",
     content: "Some useless news!",
-    glideCount: 300
+    glideCount: generateRandomNumber()
   },
   {
     category: "Movies",
     content: "Peter Jackson as the director of new Lotr.",
-    glideCount: 1000
+    glideCount: generateRandomNumber()
   }
 ]
 
@@ -37,61 +41,22 @@ const TrendsSidebar = () => {
       <div class="flex-it p-4">
         <span class="text-xl font-bold">Trends</span>
       </div>
-      <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-        <div class="flex-it">
-          <span class="text-gray-400 text-sm">
-            Trends in Europe
-          </span>
-          <span class="text-lg font-bold">Sports</span>
-          <span class="text-gray-400 text-sm">10 000 glides</span>
-        </div>
-      </div>
-      <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-        <div class="flex-it">
-          <span class="text-gray-400 text-sm">
-            Trends in Europe
-          </span>
-          <span class="text-lg font-bold">Sports</span>
-          <span class="text-gray-400 text-sm">10 000 glides</span>
-        </div>
-      </div>
-      <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-        <div class="flex-it">
-          <span class="text-gray-400 text-sm">
-            Trends in Europe
-          </span>
-          <span class="text-lg font-bold">Sports</span>
-          <span class="text-gray-400 text-sm">10 000 glides</span>
-        </div>
-      </div>
-      <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-        <div class="flex-it">
-          <span class="text-gray-400 text-sm">
-            Trends in Europe
-          </span>
-          <span class="text-lg font-bold">Sports</span>
-          <span class="text-gray-400 text-sm">10 000 glides</span>
-        </div>
-      </div>
-      <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-        <div class="flex-it">
-          <span class="text-gray-400 text-sm">
-            Trends in Europe
-          </span>
-          <span class="text-lg font-bold">Sports</span>
-          <span class="text-gray-400 text-sm">10 000 glides</span>
-        </div>
-      </div>
-      <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
-        <div class="flex-it">
-          <span class="text-gray-400 text-sm">
-            Trends in Europe
-          </span>
-          <span class="text-lg font-bold">Sports</span>
-          <span class="text-gray-400 text-sm">10 000 glides</span>
-        </div>
-      </div>
-      {JSON.stringify(trends)}
+
+      <For each={trends}>
+        {
+          (trend) =>
+            <div class="flex-it p-4 cursor-pointer transition duration-200 hover:bg-gray-700">
+              <div class="flex-it">
+                <span class="text-gray-400 text-sm">
+                  {trend.content}
+                </span>
+                <span class="text-lg font-bold">{trend.category}</span>
+                <span class="text-gray-400 text-sm">{trend.glideCount} glides</span>
+              </div>
+            </div>
+        }
+      </For>
+
     </div>
   );
 };
